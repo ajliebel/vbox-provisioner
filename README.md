29d6d44e26d3fcefc6d25b39170cbfb6029c1d16
Adrian J. Liebel <adrian@ajliebel.com>
2020-11-08 09:59:04 -0500

    steps and instructions for auto install

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # VBOX Provisioner
 
 Auto provisioning of Linux configured Virtualbox servers that are ready to run Docker and Kuberneties.
+
+# Step 1
+```
+$ ./create_centos7_vm.sh
+```
+# Step 2
+```
+$ VBoxManage startvm cl-02 --type=headless
+```
+# Step 3
+```
+$ VBoxManage showvminfo c7-02 --machinereadable | grep macaddress1
+```
+# Step 4
+```
+$ sudo arp-scan --interface=vboxnet0 --localnet
+````
--- create_centos7_vm.sh
+++ create_centos7_vm.sh
@@ -1,15 +1,16 @@
-VM='Centos-7-64-bit'
-VBoxManage createhd --filename $VM.vdi --size 32768
+VM='c7-02'
+mkdir /mnt/disk-1/shared/vbox/$VM
+VBoxManage createhd --filename /mnt/disk-1/shared/vbox/$VM/$VM.vdi --size 32768
 VBoxManage createvm --name $VM --ostype "Redhat_64" --register
 VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
-VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM.vdi
+VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium /mnt/disk-1/shared/vbox/$VM/$VM.vdi
 VBoxManage storagectl $VM --name "IDE Controller" --add ide
-VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 
-VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium /mnt/disk-1/shared/software/CentOS-7-x86_64-DVD-1810.iso
+#VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 
+VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium /mnt/disk-1/shared/software/CentOS-7-x86_64-Minimal-2003.iso
 VBoxManage modifyvm $VM --ioapic on
 VBoxManage modifyvm $VM --boot1 dvd --boot2 disk --boot3 none --boot4 none
 VBoxManage modifyvm $VM --memory 1024 --vram 128
-VBoxManage modifyvm $VM --nic1 bridged --bridgeadapter1 enp34s0
-VBoxHeadless -s $VM
-
+VBoxManage modifyvm $VM --nic1 hostonly --hostonlyadapter1 vboxnet0
+VBoxManage modifyvm $VM --nic2 bridged --bridgeadapter2 enp34s0
+VBoxManage unattended install $VM  --iso=/mnt/disk-1/shared/software/CentOS-7-x86_64-Minimal-2003.iso --user=adrian --full-user-name=Adrian --password=foobar.123
 
